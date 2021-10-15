ad34134ee33374bacf26dce5fea2ec28d2ed295d
Leonam Silva <leonam.murilo@gmail.com>
2021-10-15 16:26:37 -0300

    nlw letmeask aula 3

---
+++ src/components/RoomCode.tsx
@@ -0,0 +1,23 @@
+import copyImg from '../assets/images/copy.svg'
+
+import "../styles/room-code.scss"
+
+type RoomCodeProps = {
+    code: string;
+}
+
+
+export function RoomCode(props: RoomCodeProps) {
+    function copyRoomCodeToclipboard(){
+        navigator.clipboard.writeText(props.code)
+    }
+
+    return(
+        <button className="room-code" onClick={copyRoomCodeToclipboard}>
+            <div>
+                <img src={copyImg} alt="copy"/>
+            </div>
+            <span>Sala #{props.code}</span>
+        </button>
+    )
+}
