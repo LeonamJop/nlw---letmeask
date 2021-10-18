f7eb4bd7c044cd3fddf38193fcd13c5e3c54d03f
Leonam Silva <leonam.murilo@gmail.com>
2021-10-17 21:42:33 -0300

    nlw letmeask aula 5

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -5,13 +5,13 @@ import 'firebase/compat/database';
 
 
 export const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID
+    apiKey: "AIzaSyCCd7lWWPqwblGTqLP--kB_NVRgs73SpyY",
+    authDomain: "letmeask---nlw-6f482.firebaseapp.com",
+    databaseURL: "https://letmeask---nlw-6f482-default-rtdb.firebaseio.com",
+    projectId: "letmeask---nlw-6f482",
+    storageBucket: "letmeask---nlw-6f482.appspot.com",
+    messagingSenderId: "633934005354",
+    appId: "1:633934005354:web:f2934fde296bc83476b613"
 };
 
 firebase.initializeApp(firebaseConfig);
